398f6d42fb4e0793192e4b6dcf47687b84764f9c
52254435 <52254435@COREPW0LX6YZ>
2026-06-18 00:31:44 +0530

    commit changes

--- Scripts/Login Test/Script1781719472888.groovy
+++ Scripts/Login Test/Script1781719472888.groovy
@@ -17,5 +17,5 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-WebUI.openBrowser('www.google.com')
+WebUI.openBrowser('www.gmail.com')
 
